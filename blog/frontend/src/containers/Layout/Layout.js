6ed855767724d18ac5cd79ec3f748455b261cf18
Cornel Remer <cornel.remer@gmail.com>
2019-03-02 11:56:00 +0100

    toolbar, sidedrawer, backdrop

--- blog/frontend/src/containers/Layout/Layout.js
+++ blog/frontend/src/containers/Layout/Layout.js
@@ -1,29 +1,34 @@
 import React, { Component } from 'react';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
 
 import './Layout.css';
 
 class Layout extends Component {
     state = {
-        showToolbar: false
+        showSideDrawer: false
     }
 
-    toggleToolbarHandler = () => {
-        const toggleToolbar = !this.state.showToolbar;
-        this.setState({showToolbar: toggleToolbar});
+    SideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false})
+    }
+
+    SideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return {showSideDrawer: !this.state.showSideDrawer}
+        });
     }
 
     render () {
-        let toolbar = <Toolbar />
-        if (!this.state.showToolbar) {
-            toolbar = null
-        }
         return (
-            <div className="Layout">
-                {toolbar}
-                <button onClick={this.toggleToolbarHandler}>Toggle Toolbar</button>
-                <p>Body</p>
-                <p>Others</p>
+            <div>
+                <Toolbar drawerToggleClicked={this.SideDrawerToggleHandler}/>
+                <SideDrawer 
+                    open={this.state.showSideDrawer}
+                    closed={this.SideDrawerClosedHandler}/>
+                <div className="Layout">
+                    {this.props.children}
+                </div>
             </div>
         )
     }
