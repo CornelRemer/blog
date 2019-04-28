9bcd3ace233115423e45ff1581bb689761a0c5fd
Cornel Remer <cornel.remer@gmail.com>
2019-04-28 20:42:28 +1000

    implemented postgresql and postgis

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -91,10 +91,20 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'blog_db',
+        'USER': 'cornel',
+        'PASSWORD': 'KalteAsche430',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
