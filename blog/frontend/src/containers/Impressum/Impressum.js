a87c612fdde43c8f880cddd95dbece6fc4bec69b
Cornel Remer <cornel.remer@gmail.com>
2019-06-18 22:11:06 +1000

    Post month requests api

--- blog/frontend/src/containers/Impressum/Impressum.js
+++ blog/frontend/src/containers/Impressum/Impressum.js
@@ -5,7 +5,7 @@ const impressum = ( props ) => (
     <div className="Impressum">
         <p><strong>Impressum</strong></p>
         <p>Anbieter:<br />Cornel Remer</p>
-        <p>Kontakt:<br />Telefon: 0491 112412<br />E-Mail: cornel_remer@yahoo.de</p>
+        <p>Kontakt:<br />Telefon: +61 491 112412<br />E-Mail: cornel_remer@yahoo.de</p>
         <p> </p>
         <p>Bei redaktionellen Inhalten:</p>
         <p>Verantwortlich nach § 55 Abs.2 RStV<br />Cornel Remer</p>
